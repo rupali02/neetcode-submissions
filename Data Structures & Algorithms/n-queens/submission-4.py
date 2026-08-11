71141df254eca8071133904e72c8b3e6a89d8fd5
rupali02 <74283733+rupali02@users.noreply.github.com>
2026-08-11 19:01:41 +0530

    Add: n-queens - submission-4

---
+++ Data Structures & Algorithms/n-queens/submission-4.py
@@ -0,0 +1,43 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        grid = [("." * n) for _ in range(n)]
+        result = []
+
+        def valid(r, c, grid):
+            if "Q" in grid[r]:
+                return False
+
+            row, col = r, c
+
+            while r >= 0 and c >= 0:
+                if grid[r][c] == "Q":
+                    return False
+                r -= 1
+                c -= 1
+
+            r, c = row, col
+
+            while r < n and c >= 0:
+                if grid[r][c] == "Q":
+                    return False
+                r += 1
+                c -= 1
+
+            return True
+
+        def queens(c, grid):
+            if c == n:
+                result.append(grid[:])
+                return 
+
+            for r in range(n):
+                if valid(r, c, grid):
+                    grid[r] = grid[r][:c] + "Q" + grid[r][c+1:]
+                    queens(c+1, grid)
+                    grid[r] = grid[r][:c] + "." + grid[r][c+1:]
+
+        queens(0, grid)
+        return result
+                
+
+        
